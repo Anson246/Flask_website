74ab8cad12cd89d7bc2b44613f470b2b3abdb3a5
Joshith Meetakoti <joshith5103@gmail.com>
2023-02-18 17:31:22 +0530

    added flash messages and initialised database(without models)

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -11,7 +11,23 @@ def login():
 def blogs():
     return render_template('blogs.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
+    if request.method == 'POST':
+        email=request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category="error")
+        elif len(username) < 2:
+            flash('Username must be greater than 2 characters', category="error")
+        elif password1 != password2:
+            flash('password do not match', category='error')
+        elif len(password1) < 7:
+            flash('password must be greater than 7 characters', category='error')
+        else:
+            flash('account created!', category='success')
     return render_template('signup.html')
 
