5482ea3462b0319217af578eebf01edef4a1add0
Joshith Meetakoti <joshith5103@gmail.com>
2023-02-18 20:10:15 +0530

    adding user data to the database and logging them in

--- website/models.py
+++ website/models.py
@@ -19,6 +19,6 @@ class Blog(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
     username = db.Column(db.String(150))
+    password = db.Column(db.String(150))
     blogper = db.relationship('Blog')
